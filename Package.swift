d42c589d3ffddbac76e1095597c0541c5ee71c9e
Tiago Martinho <t.martinho@icloud.com>
2018-10-06 12:08:33 +0200

    Use new Package format

--- Package.swift
+++ Package.swift
@@ -1,5 +1,16 @@
+// swift-tools-version:4.2
+
 import PackageDescription
 
 let package = Package(
-  name: "StyleDecorator"
+    name: "StyleDecorator",
+    products: [
+        .library(name: "StyleDecorator", targets: ["StyleDecorator"])
+    ],
+    targets: [
+        .target(
+            name: "StyleDecorator",
+            dependencies: [],
+            path: "StyleDecorator")
+    ]
 )
